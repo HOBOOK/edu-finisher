3b6241fa87a41dd670977375053b7355157db046
peter <pkh879@gmail.com>
2023-06-07 09:24:31 +0900

    [MOD] 수강항목 변경에 따른 헤더 수정

--- vef-api/src/main/java/com/vazil/vef/controller/RequestController.java
+++ vef-api/src/main/java/com/vazil/vef/controller/RequestController.java
@@ -37,8 +37,8 @@ public class RequestController {
 
         for(int i = 0; i < 14; i++) {
             MultiValueMap<String, Object> form = new LinkedMultiValueMap<>();
-            form.add("p_id", "e31f75babe704036f65fc864d30e6f0e");
-            form.add("s_id", "abku");
+            form.add("p_id", "c67ec40e5a4442686367a39e92f3e791");
+            form.add("s_id", "ujeb");
             form.add("wr_order", formData.getOrder());
             form.add("wr_page", (i+1));
             form.add("wr_id", formData.getId());
@@ -50,12 +50,12 @@ public class RequestController {
 
             timestamp = String.valueOf(Timestamp.valueOf(LocalDateTime.now()).getTime() - rand).substring(0, 10);
             log.info(timestamp);
-            headers.set("Cookie", "f33d2ed86bd82d4c22123c9da444d8ab=MTY3NTQwMDI1OA==; 96b28b766b7e0699aa91c9ff3d890663=aHR0cDovL3ZhemlsLmNhbXB1c2xpbmUua3IvbG1zL2NsYXNzL3N0dWRlbnQv; PHPSESSID=6dm7rov2klvhdt7c0h75la4rc0; 2a0d2363701f23f8a75028924a3af643=NTkuMjEuMzQuMjE0; play={\"wr_id\":\"1703125\",\"p_id\":\"e31f75babe704036f65fc864d30e6f0e\",\"s_id\":\"abku\",\"wr_order\":\"5\",\"sample_mode\":false}; move_prev=false; page_time={\"key\":\"63e2e232d707c\",\"time\":" + timestamp +"}");
+            headers.set("Cookie", "f33d2ed86bd82d4c22123c9da444d8ab=MTY4NjA5Njc5Mw%3D%3D; PHPSESSID=1s4uu426fshqg2ldom4f5trmp3; 2a0d2363701f23f8a75028924a3af643=NTkuMjEuMzQuMjE0; 96b28b766b7e0699aa91c9ff3d890663=aHR0cDovL3ZhemlsLmNhbXB1c2xpbmUua3Iv; play_head=%7B%22wr_id%22%3A1831300%2C%22p_id%22%3A%22c67ec40e5a4442686367a39e92f3e791%22%2C%22s_id%22%3A%22ujeb%22%2C%22wr_order%22%3A%221%22%7D; move_prev=true; play=%7B%22wr_id%22%3A%221831300%22%2C%22p_id%22%3A%22c67ec40e5a4442686367a39e92f3e791%22%2C%22s_id%22%3A%22ujeb%22%2C%22wr_order%22%3A%221%22%2C%22sample_mode%22%3Afalse%7D; page_time=%7B%22key%22%3A%22647fcc58d71ea%22%2C%22time%22%3A" + timestamp +"%7D");
             responseEntity = restTemplate.exchange(requestUrl, HttpMethod.POST, new HttpEntity<>(form, headers), String.class);
 
             timestamp = String.valueOf(Timestamp.valueOf(LocalDateTime.now()).getTime() - (rand - add)).substring(0, 10);
             log.info(timestamp);
-            headers.set("Cookie", "f33d2ed86bd82d4c22123c9da444d8ab=MTY3NTQwMDI1OA==; 96b28b766b7e0699aa91c9ff3d890663=aHR0cDovL3ZhemlsLmNhbXB1c2xpbmUua3IvbG1zL2NsYXNzL3N0dWRlbnQv; PHPSESSID=6dm7rov2klvhdt7c0h75la4rc0; 2a0d2363701f23f8a75028924a3af643=NTkuMjEuMzQuMjE0; play={\"wr_id\":\"1703125\",\"p_id\":\"e31f75babe704036f65fc864d30e6f0e\",\"s_id\":\"abku\",\"wr_order\":\"5\",\"sample_mode\":false}; move_prev=false; page_time={\"key\":\"63e2e232d707c\",\"time\":" + timestamp +"}");
+            headers.set("Cookie", "f33d2ed86bd82d4c22123c9da444d8ab=MTY4NjA5Njc5Mw%3D%3D; PHPSESSID=1s4uu426fshqg2ldom4f5trmp3; 2a0d2363701f23f8a75028924a3af643=NTkuMjEuMzQuMjE0; 96b28b766b7e0699aa91c9ff3d890663=aHR0cDovL3ZhemlsLmNhbXB1c2xpbmUua3Iv; play_head=%7B%22wr_id%22%3A1831300%2C%22p_id%22%3A%22c67ec40e5a4442686367a39e92f3e791%22%2C%22s_id%22%3A%22ujeb%22%2C%22wr_order%22%3A%221%22%7D; move_prev=true; play=%7B%22wr_id%22%3A%221831300%22%2C%22p_id%22%3A%22c67ec40e5a4442686367a39e92f3e791%22%2C%22s_id%22%3A%22ujeb%22%2C%22wr_order%22%3A%221%22%2C%22sample_mode%22%3Afalse%7D; page_time=%7B%22key%22%3A%22647fcc58d71ea%22%2C%22time%22%3A" + timestamp +"%7D");
             responseEntity = restTemplate.exchange(requestUrl, HttpMethod.POST, new HttpEntity<>(form, headers), String.class);
         }
 
